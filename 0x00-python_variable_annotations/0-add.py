539c77232aa49fd45070c71b7bbf910ef4c42296
niyioo <niyiolagbegi@gmail.com>
2024-01-11 04:20:00 -0800

    first commit with first task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module with type-annotated function add.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Function that takes two float arguments and returns their sum as a float.
+
+    Parameters:
+        a (float): The first input number.
+        b (float): The second input number.
+
+    Returns:
+        float: The sum of the input numbers.
+    """
+    return a + b
