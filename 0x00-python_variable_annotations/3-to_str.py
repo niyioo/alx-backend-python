fd0f48aca1d2c7cf8fbaf25a9f478f66fa5cab92
niyioo <niyiolagbegi@gmail.com>
2024-01-11 04:37:36 -0800

    task 3 to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module with type-annotated function to_str.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that takes a float argument and
+    returns its string representation.
+
+    Parameters:
+        n (float): The input float number.
+
+    Returns:
+        str: The string representation of the input float number.
+    """
+    return str(n)
